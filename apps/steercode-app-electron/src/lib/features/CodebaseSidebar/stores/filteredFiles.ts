775e8b38d55ecff55c379561294f9b514163bee5
Martin Votruba <martin@nftscoring.com>
2023-05-30 15:38:19 +0200

    improve search to include path match

--- apps/steercode-app-electron/src/lib/features/CodebaseSidebar/stores/filteredFiles.ts
+++ apps/steercode-app-electron/src/lib/features/CodebaseSidebar/stores/filteredFiles.ts
@@ -4,20 +4,39 @@ import { writable } from 'svelte/store';
 function createFilteredFilesStore() {
   const _filteredFiles = writable<IFileTreeItem | null>(null);
 
+  function expandRecursively(fileTree: IFileTreeItem) {
+    if (fileTree.isDirectory) {
+      fileTree.expanded = true;
+      for (const child of fileTree.children) {
+        expandRecursively(child);
+      }
+    }
+  }
+
   function filterChildren(
     fileTree: IFileTreeItem,
     query: string
   ): IFileTreeItem {
     const filteredChildren: IFileTreeItem[] = [];
 
-    for (const child of fileTree.children) {
-      if (child.isDirectory) {
-        const filteredChild = filterChildren(child, query);
-        if (filteredChild.children.length > 0) {
-          filteredChildren.push(filteredChild);
+    if (
+      fileTree.filePath.toLowerCase().includes(query.toLowerCase()) &&
+      fileTree.isDirectory
+    ) {
+      // expand all children
+      expandRecursively(fileTree);
+
+      filteredChildren.push(...fileTree.children);
+    } else {
+      for (const child of fileTree.children) {
+        if (child.isDirectory) {
+          const filteredChild = filterChildren(child, query);
+          if (filteredChild.children.length > 0) {
+            filteredChildren.push(filteredChild);
+          }
+        } else if (child.fileName.toLowerCase().includes(query.toLowerCase())) {
+          filteredChildren.push(child);
         }
-      } else if (child.fileName.toLowerCase().includes(query.toLowerCase())) {
-        filteredChildren.push(child);
       }
     }
 
